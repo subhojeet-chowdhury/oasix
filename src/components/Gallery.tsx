ffa10e3b17fa7a40ac1ca3aa7b1e58e5125bccf8
subhojeet chowdhury <subhojeetchowdhury98@gmail.com>
2024-07-20 02:41:56 +0530

    refactored code

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,18 +5,20 @@ const Gallery = () => {
   return (
     <section className="galleries-container">
       <h2>Explore the functions of the marketplace</h2>
-      <div className="galleries">
-        {galleryData.map((gallery: GalleryItem) => (
-          <div className="gallery">
-            <div className="gallery-image">
-              <img src={gallery.image} alt="Daria" />
+      <div className="galleries-wrapper">
+        <div className="galleries">
+          {galleryData.map((gallery: GalleryItem, index: number) => (
+            <div className="gallery" key={index}>
+              <div className="gallery-image">
+                <img src={gallery.image} alt={gallery.heading} />
+              </div>
+              <div className="gallery-content">
+                <h3>{gallery.heading}</h3>
+                <p>{gallery.description}</p>
+              </div>
             </div>
-            <div className="gallery-content">
-              <h3>{gallery.heading}</h3>
-              <p>{gallery.description}</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       <button>
         Go to marketplace <i className="fa-solid fa-arrow-right"></i>
